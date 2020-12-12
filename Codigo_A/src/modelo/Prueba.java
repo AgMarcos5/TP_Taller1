b8bdf919646d76354b697caf3df0e56c9821a228
Marcos Aguero <aguermar@gmail.com>
2020-12-12 17:06:58 -0300

    programación defensiva

---
+++ Codigo_A/src/modelo/Prueba.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sistema sistema = Sistema.getInstancia();
+		
+		sistema.listarFacturas();
+	}
+
+}
